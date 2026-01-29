27f8a8385188c7079652d3526a118ee4d8d21a6e
HarshitV <harshit15037@iiitd.ac.in>
2026-01-29 20:02:18 +0530

    Revert "Update versions, add lining and formatting"
    This reverts commit b88743604f89c09050744bea2e8f850d99f7c3bf.

--- src/components/FullWidthContainer.tsx
+++ src/components/FullWidthContainer.tsx
@@ -1,34 +1,38 @@
-import { JSX } from "react";
-import styled from "styled-components";
+import styled from 'styled-components';
+import { breakpoints } from 'styles/constants';
 
 interface Props {
-    children: JSX.Element;
-    flexOnRow?: boolean;
-    display?: string;
+  children: JSX.Element;
+  flexOnRow?: boolean;
+  display?: string;
 }
 
-const FullWidthContainer = ({ children, flexOnRow = false, display = "flex" }: Props) => {
-    return (
-        <Container flexOnRow={flexOnRow} display={display}>
-            {children}
-        </Container>
-    );
+const FullWidthContainer = ({
+  children,
+  flexOnRow = false,
+  display = 'flex',
+}: Props) => {
+  return (
+    <Container flexOnRow={flexOnRow} display={display}>
+      {children}
+    </Container>
+  );
 };
 
 export default FullWidthContainer;
 
 const Container = styled.div<{ flexOnRow: boolean; display: string }>`
-    width: 100vw;
-    padding: 40px 20px;
-    background: rgb(247, 247, 247);
-    display: ${({ display }) => display && display};
-    flex-direction: ${({ flexOnRow }) => (flexOnRow ? "row" : "column")};
-    justify-content: center;
-    align-items: center;
-    p,
-    h1,
-    h2 {
-        text-align: center;
-    }
-    margin-bottom: 40px;
+  width: 100vw;
+  padding: 40px 20px;
+  background: rgb(247, 247, 247);
+  display: ${({ display }) => display && display};
+  flex-direction: ${({ flexOnRow }) => (flexOnRow ? 'row' : 'column')};
+  justify-content: center;
+  align-items: center;
+  p,
+  h1,
+  h2 {
+    text-align: center;
+  }
+  margin-bottom: 40px;
 `;
--- src/components/Navigation/TopNav.tsx
+++ src/components/Navigation/TopNav.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from "next";
-import TopNavDesktop from "./TopNavDesktop";
-import TopNavMobile from "./TopNavMobile";
+import type { NextPage } from 'next';
+import TopNavDesktop from './TopNavDesktop';
+import TopNavMobile from './TopNavMobile';
 
 const TopNav: NextPage = () => (
-    <>
-        <TopNavDesktop />
-        <TopNavMobile />
-    </>
+  <>
+    <TopNavDesktop />
+    <TopNavMobile />
+  </>
 );
 export default TopNav;
